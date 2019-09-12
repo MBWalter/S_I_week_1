82a535b59dc373a43ebf42d50d56cb1680cafbcf
MBWalter <54991386+MBWalter@users.noreply.github.com>
2019-09-12 19:40:25 +0200

    Create Dice_for_risk_norm.py

---
+++ Dice_for_risk_norm.py
@@ -0,0 +1,10 @@
+import random
+
+rolls_value=[]
+rolls=5
+for i in range(rolls):
+    dice = random.randint(1,6)
+    rolls_value.append(dice)
+print("Dice:")
+print("Attacker: " + str(rolls_value[0]) + "-" + str(rolls_value[1]) + "-" + str(rolls_value[2]))
+print("Defender: " + str(rolls_value[3]) + "-" + str(rolls_value[4]))
