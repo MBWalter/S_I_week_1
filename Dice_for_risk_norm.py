9412a66aad5c4d7bc1cce06e5df2206cbb264b0a
MBWalter <54991386+MBWalter@users.noreply.github.com>
2019-09-12 19:42:34 +0200

    Update Dice_for_risk_norm.py

--- Dice_for_risk_norm.py
+++ Dice_for_risk_norm.py
@@ -6,5 +6,5 @@ for i in range(rolls):
     dice = random.randint(1,6)
     rolls_value.append(dice)
 print("Dice:")
-print("Attacker: " + str(rolls_value[0]) + "-" + str(rolls_value[1]) + "-" + str(rolls_value[2]))
-print("Defender: " + str(rolls_value[3]) + "-" + str(rolls_value[4]))
+print("  Attacker: " + str(rolls_value[0]) + "-" + str(rolls_value[1]) + "-" + str(rolls_value[2]))
+print("  Defender: " + str(rolls_value[3]) + "-" + str(rolls_value[4]))
